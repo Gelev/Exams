fdbc485b5334cea4ea51dedd464ca73c6120acd9
Gelev <dragomir.gelev@gmail.com>
2017-11-08 20:56:49 +0200

    Part 1 - 100/100

---
+++ C#Fundamentals Exam 09 July 2017/Problem 1. Poke Mon/Problem 1. Poke Mon/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int pokePower = int.Parse(Console.ReadLine());
+        int distanceBetweenTargets = int.Parse(Console.ReadLine());
+        int exhaustionFactor = int.Parse(Console.ReadLine());
+        int halfPokePower = -1;
+
+        if ((pokePower % 2) == 0)
+        {
+            halfPokePower = pokePower / 2;
+        }
+
+        int pokeCounter = 0;
+
+        for (int i = pokePower; i >= 0 ; i-= distanceBetweenTargets)
+        {
+            if (i == halfPokePower && exhaustionFactor != 0)
+            {
+                i /= exhaustionFactor;
+            }
+            if ((i - distanceBetweenTargets) >= 0)
+            {
+                pokeCounter++;
+            }
+            pokePower = i;
+
+        }
+
+        Console.WriteLine(pokePower);
+        Console.WriteLine(pokeCounter);
+
+    }
+}
+
---
+++ C#Fundamentals Exam 09 July 2017/Problem 2. Pokemon Don't Go/Problem 2. Pokemon Don't Go/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int[] output = input;
+        int sum = 0;
+
+        while (output.Length > 0)
+        {
+            int index = int.Parse(Console.ReadLine());
+            int elementToRemove = 0;
+            
+            if (index >= 0 && index < output.Length)
+            {
+                elementToRemove = output[index];
+                sum += elementToRemove;
+                output = output.Where((source, ind) => ind != index).ToArray();
+            }
+            if (index < 0)
+            {
+                index = 0;
+                elementToRemove = output[index];
+                sum += elementToRemove;
+                output[index] = output[output.Length - 1];
+            }
+            if (index > output.Length)
+            {
+                index = output.Length - 1;
+                elementToRemove = output[index];
+                sum += elementToRemove;
+                output[index] = output[0];
+            }
+
+            
+            //updating the remaining values of the array
+            for (int j = 0; j < output.Length; j++)
+            {
+                
+                if (output[j] <= elementToRemove)
+                {
+                    output[j] += elementToRemove;
+                }
+                else
+                {
+                    output[j] -= elementToRemove;
+                }
+            }
+        }
+        Console.WriteLine(sum);
+   }
+}
+
+
